908072ed2204958d221e4ec4e9a59cbd732cb665
Vish Mohan <viswanath.mohan@gmail.com>
2023-01-31 22:23:01 +0000

    centering text challenge

--- src/01_02/centering_challenge.c
+++ src/01_02/centering_challenge.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
 void center_text(int width, char *text)
 {
 	/*
-	   - write this function to complete the challenge
-	   - this code won't run until the function is
-	   written
+		 - write this function to complete the challenge
+		 - this code won't run until the function is
+		 written
 	*/
+	char space = ' ';
+	int mlen = strlen(text);
+	int start = (width - mlen) / 2;
+
+	int i;
+	for (i = 0; i < start; ++i)
+		putchar(space);
+	printf("%s\n", text);
 }
 
 int main()
 {
 	/* some sample text to center */
 	char *title[] = {
-	   "March Sales",
-	   "My First Project",
-	   "Centering output is so much fun!",
-	   "This title is very long, just to see whether the code can handle such a long title"
-	};
+			"March Sales",
+			"My First Project",
+			"Centering output is so much fun!",
+			"This title is very long, just to see whether the code can handle such a long title"};
 	int x;
 
 	/* output each of the strings, centered */
-	for( x=0; x<4; x++ )
+	for (x = 0; x < 4; x++)
 	{
 		/* assume the screen width to be 80 characters */
-		center_text( 80, title[x] );
+		center_text(80, title[x]);
 	}
 
-	return(0);
+	return (0);
 }
